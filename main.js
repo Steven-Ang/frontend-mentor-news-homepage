d613a353ac7d9cc02676f56c224dfc9bbbf5e832
Steven Ang <spikegreen2@gmail.com>
2024-10-14 23:09:35 +0800

    Add hamburger menu

---
+++ main.js
@@ -0,0 +1,46 @@
+const body = document.querySelector("body");
+const openMenuButton = document.getElementById("open-menu-button");
+const closeMenuButton = document.getElementById("close-menu-button");
+const navigationContent = document.getElementById("navigation-content");
+
+const bodyScrollLock = bodyScrollLockUpgrade;
+
+const media = window.matchMedia("(width < 42em)");
+
+const openMenu = () => {
+  openMenuButton.setAttribute("aria-expanded", "true");
+  closeMenuButton.setAttribute("aria-hidden", "false");
+  navigationContent.removeAttribute("inert");
+
+  bodyScrollLock.disableBodyScroll(body);
+
+  closeMenuButton.focus();
+};
+
+const closeMenu = () => {
+  openMenuButton.setAttribute("aria-expanded", "false");
+  closeMenuButton.setAttribute("aria-hidden", "true");
+  navigationContent.setAttribute("inert", "");
+
+  bodyScrollLock.enableBodyScroll(body);
+
+  openMenuButton.focus();
+};
+
+const handleResize = () => {
+  if (media.matches) {
+    openMenuButton.setAttribute("aria-expanded", "false");
+    openMenuButton.setAttribute("aria-hidden", "false");
+    closeMenuButton.setAttribute("aria-hidden", "true");
+    navigationContent.setAttribute("inert", "");
+  } else {
+    openMenuButton.setAttribute("aria-expanded", "false");
+    openMenuButton.setAttribute("aria-hidden", "true");
+    closeMenuButton.setAttribute("aria-hidden", "true");
+    navigationContent.removeAttribute("inert");
+  }
+};
+
+openMenuButton.addEventListener("click", openMenu);
+closeMenuButton.addEventListener("click", closeMenu);
+media.addEventListener("change", handleResize);
